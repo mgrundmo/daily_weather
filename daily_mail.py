2d2d648be5ccc8cda548613d22d879e92914a0f1
mgrundmo <mgrundmo@gmail.com>
2025-01-19 21:03:15 +0100

    multi user functionality implemented

--- daily_mail.py
+++ daily_mail.py
@@ -11,42 +11,46 @@ receiver_email = "mgrundmo@gmx.de"
 location = "Hürth"
 
 # list of adresses
-adresses = [{'email': 'mgrundmo@gmx.de', 'location': 'Hürth'}, {'email': 'michael.grund@gmx.org', 'location': 'Bangkok'}]
-
-for adresse in adresses:
-    for i in adresse:
-        print(adresse[i])
-
-weather = forecast_weather(location)
-moon_phase_de = moon(weather['moon_phase'])
+adresses = [{'email': 'mgrundmo@gmx.de', 'location': 'Hürth'}, {'email': 'michael.grund@gmx.org', 'location': 'Bangkok'}, {'email': 'sissi-michael@gmx.de', 'location': 'Sürth'}]
 
 #getting todays date and weekday
 today = date.today()
 weekday = today.strftime("%A, %d %B %Y")
 
-#creating header of email
-msg = MIMEMultipart('alternative')
-msg['Subject'] = "Das Wetter für " +str(location) +" am " +str(weekday)
-msg['From'] = email
-msg['To'] = receiver_email
+for adresse in adresses:
+    for i in range(0, len(adresse), 2):
+        receiver_email = adresse['email']
+        print(receiver_email)
+        location = adresse['location']
+        weather = forecast_weather(location)
+        moon_phase_de = moon(weather['moon_phase'])
+        
+        #plain text email as backup
+        text = email_text(weather, location, weekday)
+        print(text)
 
-#plain text email as backup
-text = email_text(weather, location, weekday)
+        #html email as primary
+        html = email_html(weather, location, weekday)
 
-#html email as primary
-html = email_html(weather, location, weekday)
+        #creating header of email
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Das Wetter für " +str(location) +" am " +str(weekday)
+        msg['From'] = email
+        msg['To'] = receiver_email
 
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
 
-msg.attach(part1)
-msg.attach(part2)
-print(text)
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.ehlo()
-server.starttls()
-server.login(email, "xmtnrolasszlaihl")
+        msg.attach(part1)
+        msg.attach(part2)
 
-server.sendmail(email, receiver_email, msg.as_string())
+
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(email, "xmtnrolasszlaihl")
+        server.sendmail(email, receiver_email, msg.as_string())
+        #server.quit()
+        print("Email has been sent to " + receiver_email)
+
 server.quit()
-print("Email has been sent to " + receiver_email)
